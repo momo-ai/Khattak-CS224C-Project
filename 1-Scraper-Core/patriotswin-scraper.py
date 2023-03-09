24d6226f98cd788c4e8843119c5e36b9a6da81dc
momo-ai <62407942+momo-ai@users.noreply.github.com>
2023-03-08 18:12:42 -0800

    parse usernames

--- 1-Scraper-Core/patriotswin-scraper.py
+++ 1-Scraper-Core/patriotswin-scraper.py
@@ -5,15 +5,50 @@ import pandas as pd
 import time
 
 URL = 'https://patriots.win/top?sort=all'
+"""
+DIV - Main page (https://patriots.win/top?from=16aT36KFQA&sort=week)
 
-tot_comments = 0
-file = open('./patriots_win.csv', 'rb')
-curr_data = pd.read_csv(file)
-file2 = open('./patriots_win2.csv', 'rb')
-curr_data2 = pd.read_csv(file2)
+<div class=“wrapper home panel”
+	<div class =“container”
+		<main class=“main
+			<div class=“main-content post-list”
+				<div class=“post mobile_guest” data-type=“post”
+					<div class=“body”
+						<div class=“top”
+							<a href
+<div class=“more”
 
-curr_titles = curr_data["Title"].to_list()
-curr_titles.append(curr_data2["Title"].to_list())
+DIV - Given Post
+
+<article class=‘main-content’
+	<div class=‘post’
+		<div class=‘body’
+			<div class=‘top’
+				<a href text is TITLE
+<div class=‘comment-section’
+	<div class=“total” // text is Comments (NUM)
+	<section class=‘comment-list’
+		<div class=‘comment’ data-author=‘author’
+			<div class=‘vote’
+				<span class=‘count’ // could be useful metadata
+			<div class=‘body’
+				<div class=‘details’
+					<div class=‘author’ // compile these is cool
+				<div class=‘content’
+					<p // contains content
+			<div class=‘child’ // recursive case, child contains comment class which goes on
+"""
+
+
+
+# tot_comments = 0
+# file = open('./patriots_win.csv', 'rb')
+# curr_data = pd.read_csv(file)
+# file2 = open('./patriots_win2.csv', 'rb')
+# curr_data2 = pd.read_csv(file2)
+
+# curr_titles = curr_data["Title"].to_list()
+# curr_titles.append(curr_data2["Title"].to_list())
 
 browser = webdriver.Safari()
 browser.get(URL)
@@ -43,31 +78,44 @@ def extract_comments(webpage, link):
     #     browser.execute_script("arguments[0].click()", button)
     #     time.sleep(3)
     content = BeautifulSoup(browser.page_source, "html.parser")
-    all_comments = content.find_all("div", class_="comment")
+    all_comments = content.find_all("div", class_="details") ## edited here to get
     for comment in all_comments:
-        comment_proper = comment.find("div", "content")
-        if (comment_proper != None):
-            comments.append(comment_proper.text)
+        comment_proper = comment.find("div", "author") ## edit made here, replace 'content' with 'author' so we can collect usernames
+        comments.append(comment_proper.getAttribute('data-author').text)
     return comments
 
-posts_dict = {"Title": [],
-            "Post URL": [],
-            "Post Comments": []
-            }
+def extract_authors(webpage, link):
+    deadass_url = "https://patriots.win" + link
+    browser.get(deadass_url)
+    content = BeautifulSoup(browser.page_source, "html.parser")
+    return set([item.get_attribute("data-author") for item in browser.find_elements_by_css_selector("div[data-author]")])
+
+# posts_dict = {"Title": [],
+#             "Post URL": [],
+#             "Post Comments": []
+#             }
+
+authors = set()
 
 for link in links:
-    if link.text in curr_titles:
-        continue
-    posts_dict["Title"].append(link.text)
-    posts_dict["Post URL"].append(link["href"])
-    comments = extract_comments(browser, link["href"])
-    tot_comments += len(comments)
-    print(tot_comments)
-    posts_dict["Post Comments"].append(comments)
+    authors = authors.union(extract_authors(browser, link["href"]))
+    # # if link.text in curr_titles:
+    # #     continue
+    # # posts_dict["Title"].append(link.text)
+    # # posts_dict["Post URL"].append(link["href"])
+    # # comments = extract_comments(browser, link["href"])
+    # # tot_comments += len(comments)
+    # # print(tot_comments)
+    # # posts_dict["Post Comments"].append(comments)
+    # all_authors = extract_comments(browser, link["href"])
 
-top_posts = pd.DataFrame(posts_dict)
+# top_posts = pd.DataFrame(authors)
 
-print(tot_comments)
+foo = pd.DataFrame(authors)
+name = 'patriots_win_users.csv'
+foo.to_csv(name, index=True)
 
-name = 'patriots_win_ALLTIME' + '.csv'
-top_posts.to_csv(name, index=True)
+# print(tot_comments)
+
+# name = 'patriots_win_ALLTIME' + '.csv'
+# top_posts.to_csv(name, index=True)
